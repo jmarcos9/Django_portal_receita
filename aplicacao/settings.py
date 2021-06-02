78100ac8a701f64eab6068788c6f21b0ea9ab7dc
jmarcos9 <66381477+jmarcos9@users.noreply.github.com>
2021-06-02 20:01:07 -0300

    settings em templates definido local para carregar arquivos. Definido também local para localizar arquivos staticos. criado pasta static na aplicaçao para carregar staticos

--- aplicacao/settings.py
+++ aplicacao/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,7 +44,7 @@ ROOT_URLCONF = 'aplicacao.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'receitas/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,6 +108,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'aplicacao/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
